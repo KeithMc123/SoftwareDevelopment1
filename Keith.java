52c6ede97aeab1ea6d5975a72a9af0a979498e01
KeithMc123 <72506330+KeithMc123@users.noreply.github.com>
2020-11-19 02:49:36 +0000

    Update Keith.java

--- Keith.java
+++ Keith.java
@@ -1,1 +1,15 @@
+public class Keith{
+    public static void main(String[] args){
+       
+        String Name = "Keith";
+        String fband = "WestLife";
+
+        int age = 48;
+
+        System.out.println(Name + "is" + age + "years old. His favourite band is" + fband );
+
+
+
+    }
+}
 
