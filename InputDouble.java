b90254c071e2a7a2fd77fd11af4cc068c670c417
KeithMc123 <72506330+KeithMc123@users.noreply.github.com>
2020-11-19 06:20:21 +0000

    Update InputDouble.java

--- InputDouble.java
+++ InputDouble.java
@@ -1,1 +1,17 @@
+import java.util.Scanner;
+
+public class InputDouble{
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter a decimal number:");
+        double decimal = input.nextDouble();
+
+        System.out.println("Number entered is: " + decimal );
+
+        input.close();
+
+    }
+}
 
