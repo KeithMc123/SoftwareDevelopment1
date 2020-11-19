6a18b22ff744dfa10bceab65400cecfa1c37c803
KeithMc123 <72506330+KeithMc123@users.noreply.github.com>
2020-11-19 03:06:04 +0000

    Update Jean.java

--- Jean.java
+++ Jean.java
@@ -1,5 +1,14 @@
 public class Jean{
-  public static void main(String[] args){
+    public static void main(String[] args){
 
+        //“Jean is 19 years old and is studying Maths in DCU. Some day she hopes to be a teacher.”
+        String name = "Jean";
+        String course = "maths";
+        String college = "DCU";
+        String career = "teacher";
+        int age = 19;
 
-//“Jeanis 19years old and is studying Mathsin DCU. Some day she hopes to be a teacher.”
+        System.out.println( name+ " is " +age+ "years old and is studying" +course+ "in" +college+ ". Some day she hopes to be a" +career+ ".");
+
+    }
+}
