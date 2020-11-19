c6ff9fbc1b07f975c3b4bbfe509468059ac4f33e
KeithMc123 <72506330+KeithMc123@users.noreply.github.com>
2020-11-19 06:39:27 +0000

    Update Inputs.java

--- Inputs.java
+++ Inputs.java
@@ -1,1 +1,22 @@
+import java.util.Scanner;
+
+public class Inputs{
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter your first name here:");
+        String name = input.next();
+
+        System.out.println("Enter a whole number: ");
+        int number = input.nextInt();
+
+        System.out.println("Enter a decimal number:");
+        double decimal = input.nextDouble();
+
+        System.out.println(name+ ", you entered the numbers: " +number+ ", " +decimal );
+
+        input.close();
+    }
+}
 
