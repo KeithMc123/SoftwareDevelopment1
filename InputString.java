f11275278ae36627ac76350f1cbfd647c307b55a
KeithMc123 <72506330+KeithMc123@users.noreply.github.com>
2020-11-19 06:02:21 +0000

    Update InputString.java

--- InputString.java
+++ InputString.java
@@ -1,1 +1,15 @@
+import java.util.Scanner;
+
+public class InputString{
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        String name = input.next();
+        System.out.println("Enter your first name here:");
+
+        System.out.println("Hello " + name );
+
+    }
+}
 
