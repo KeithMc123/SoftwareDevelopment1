d5d60486a4240f1e995282c27d4a7619e6d4e1ba
KeithMc123 <72506330+KeithMc123@users.noreply.github.com>
2020-11-19 23:19:29 +0000

    Update Names.java

--- Names.java
+++ Names.java
@@ -1,1 +1,18 @@
+import java.util.Scanner;
 
+public class Names{
+    public public static void main(String[] args) {
+
+            Scanner input = new Scanner(System.in);
+
+            System.out.println("Enter your first name here:");
+            String name = input.next();
+            System.out.println("Enter your second name here:");
+            String name2 = input.next();
+
+            System.out.println(name);
+            System.out.println(name2);
+
+        input.close();
+    }
+}
