e174e2a57fe548554c7cf75b308d4302df468055
KeithMc123 <72506330+KeithMc123@users.noreply.github.com>
2021-02-22 06:09:27 +0000

    Add files via upload

---
+++ Wk13/Fourthloop.java
@@ -0,0 +1,13 @@
+package Wk13;
+
+public class Fourthloop{
+  public static void main(String[] args){
+
+    int counter = 1;
+    while(counter <= 10){
+        System.out.println("Loop Counter: "+counter);
+        counter++;
+    }
+
+  }
+}
---
+++ Wk13/Thirdloop.java
@@ -0,0 +1,13 @@
+package Wk13;
+
+public class Thirdloop{
+  public static void main(String[] args){
+
+    int counter = 1;
+    while(counter <= 10){
+        System.out.println(counter+" . Keith");
+        counter++;
+    }
+
+  }
+}
