aa96e55dfe1e78e0825c9fb8abcaa082afd3d363
KeithMc123 <72506330+KeithMc123@users.noreply.github.com>
2021-04-25 22:59:15 +0100

    Add files via upload

--- Exercise1.java
+++ Exercise1.java
@@ -1,37 +1,11 @@
-/*
-Program Name: 
-Inputs Required: yes    
-Variables/Types: int num1, int num2, int num3, int num4, int num5, int num6, int num7, int num8, int result
-If/else statements: no
-Loops: no
-Loop how many times: not applicable
-*/
-
-//uncomment next line if input required
-import java.util.Scanner;
-
+package Wk21;
 public class Exercise1{
-  public static void main(String[] args){
-    //uncomment next line if input required
-    Scanner input = new Scanner(System.in);
+    public static void main(String[] args){
+        int age = 28;
+        String name = "James";
+        double weight = 75.48;
 
-    int num1 = 0;
-    int num2 = 0; 
-    int num3 = 0; 
-    int num4 = 0; 
-    int num5 = 0;
-    int num6 = 0;
-    int num7 = 0; 
-    int num8 = 0; 
-    int result = 0;
-
-    System.out.print("Enter number 1: ");
-    num1 = input.nextInt();
-    
-    //Add code here for Inputs, if/else statements, loops
-
- 
-    //uncomment next line if input required
-    input.close();
-  }
-} 
+        System.out.println(name + " is " + age + " years old.");
+        System.out.println("He weighs " + weight + " kilograms.");
+    }
+}
