407ec202ec3d000812f1456a67c2c7a8cc45ac3d
KeithMc123 <72506330+KeithMc123@users.noreply.github.com>
2021-04-25 22:59:42 +0100

    Add files via upload

---
+++ Wk21/Exercise1.java
@@ -0,0 +1,11 @@
+package Wk21;
+public class Exercise1{
+    public static void main(String[] args){
+        int age = 28;
+        String name = "James";
+        double weight = 75.48;
+
+        System.out.println(name + " is " + age + " years old.");
+        System.out.println("He weighs " + weight + " kilograms.");
+    }
+}
